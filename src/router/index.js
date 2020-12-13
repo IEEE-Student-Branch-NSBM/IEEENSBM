7c45f53809b5a0f2868a46a08293924a6ad0c935
Srilal Sachintha <SrilalSachintha@Gmail.com>
2020-12-13 22:32:10 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MainContent from "@/components/MainContent";
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: MainContent
   },
   {
     path: '/about',
