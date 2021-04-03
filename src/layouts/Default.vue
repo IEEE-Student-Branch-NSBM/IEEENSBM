151ca18f99edabb3b0a7fcb8c79951be6dc2c402
Srilal Sachintha <SrilalSachintha@Gmail.com>
2021-04-04 00:41:26 +0530

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,23 +2,23 @@
   <v-app>
     <header class="header">
       <v-app-bar
-          v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }"
+          v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }"
           app
           dense
       >
         <v-row class="hidden-sm-and-down">
-          <v-btn v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }"elevation="0" dark>IEEE.org</v-btn>
-          <v-btn v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }" elevation="0" dark>IEEE Xplore Digital Library</v-btn>
-          <v-btn v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }" elevation="0" dark>IEEE Standards</v-btn>
-          <v-btn v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }" elevation="0" dark>IEEE Spectrum</v-btn>
-          <v-btn v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }" elevation="0" dark>More Sites</v-btn>
+          <v-btn v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }"elevation="0" dark>IEEE.org</v-btn>
+          <v-btn v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }" elevation="0" dark>IEEE Xplore Digital Library</v-btn>
+          <v-btn v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }" elevation="0" dark>IEEE Standards</v-btn>
+          <v-btn v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }" elevation="0" dark>IEEE Spectrum</v-btn>
+          <v-btn v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }" elevation="0" dark>More Sites</v-btn>
         </v-row>
       </v-app-bar>
     </header>
 
 
       <v-main class="grey lighten-2">
-        <v-card v-bind:class="{ WIE : $route.path ==='/wie', IEEE : $route.path !== '/wie' }" class="ml-4 mr-4">
+        <v-card v-bind:class="{ WIE : Path ==='wie', IEEE : Path !== 'wie' }" class="ml-4 mr-4">
           <v-card-title>
             <v-row no-gutters justify="space-between" align="center">
               <g-image alt="NSBM IEEE Student" src="~/assets/Logos/IEEE_SB_Logo.png" quality="100" blur="0" width="256"/>
@@ -26,7 +26,7 @@
             </v-row>
 
           </v-card-title>
-          <v-tabs :background-color="$route.path ==='/wie' ? '#480c5c':'#00629B'" dark slider-size="4" class="hidden-sm-and-down IEEE">
+          <v-tabs :background-color="$route.path.replaceAll('/','') ==='wie' ? '#702f8a':'#00629B'" dark slider-size="4" class="hidden-sm-and-down IEEE">
             <v-tab to="/">Home</v-tab>
             <v-tab to="/wie">WIE</v-tab>
             <v-tab to="/blog">Blog</v-tab>
@@ -46,11 +46,11 @@
 export default {
   data(){
     return {
-
+      Path:''
     }
   },
   mounted() {
-    console.log(this.$route.path);
+    this.Path = this.$route.path.replaceAll('/','');
   }
 }
 </script>
@@ -80,11 +80,11 @@ body {
 }
 
 .WIE {
-  background-color: #480c5c !important;
+  background-color: #702f8a !important;
 }
 
 .WIEFont {
-  color: #480c5c !important;
+  color: #702f8a !important;
 }
 
 .layout {
