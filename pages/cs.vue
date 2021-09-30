3f05f73fc3b413fb3b9e9dc5eb2b2da9eafc7291
Srilal Sachintha <SrilalSachintha@Gmail.com>
2021-10-01 00:41:49 +0530

    Update cs.vue

--- pages/cs.vue
+++ pages/cs.vue
@@ -1,5 +1,15 @@
 <template>
-
+  <v-container>
+    <v-row justify="center" no-gutters>
+      <v-card class="rounded-circle" rounded width="128" height="128">
+        <nuxt-img
+          alt="CS logo"
+          quality="96"
+          format="webp"
+          class="rounded-circle" height="128" width="128"  src="/Assets/Logos/CS_Logo.png"></nuxt-img>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
