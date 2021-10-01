0df1d9c1ba3d86fce4223c14660f358fa33438c9
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2021-10-02 04:23:36 +0530

    Adding CS Page Title and Intro Content adding

--- docs/_nuxt/static/1633036990/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontact","\u002Fblog","\u002Fcs","\u002Fwie","\u002Fmembership","\u002Fvolunteer","\u002Fevents"]})
--- pages/cs.vue
+++ pages/cs.vue
@@ -7,8 +7,31 @@
           quality="96"
           format="webp"
           class="rounded-circle" height="128" width="128"  src="/Assets/Logos/CS_Logo.png"></nuxt-img>
+
       </v-card>
+
     </v-row>
+    <v-row no-gutters justify="center">
+      <h1 class="CSFont">
+        IEEE Computer Society
+      </h1>
+    </v-row>
+    <v-row no-gutters justify="center">
+      <v-card-title class="text-center">IEEE NSBM Computer Society</v-card-title>
+    </v-row>
+
+    <v-col>
+      <h1 class="text-center CSFont">Who Are We?</h1>
+      <p class="text-justify">
+        The IEEE Computer Society is the premier source for information, inspiration, and
+        collaboration in computer science and engineering. Connecting members worldwide,
+        the Computer Society empowers the people who advance technology by delivering tools for individuals
+        at all stages of their professional careers. Our trusted resources include international conferences,
+        peer-reviewed publications, a robust digital library, globally recognized standards,
+        and continuous learning opportunities.
+      </p>
+    </v-col>
+
   </v-container>
 </template>
 
@@ -21,3 +44,4 @@ export default {
 <style scoped>
 
 </style>
+
