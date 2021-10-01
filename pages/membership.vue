14cab024c3c71d8a39822da2be17933d741e7db7
Isura Dilshan <64422787+IsuraDilshan@users.noreply.github.com>
2021-10-01 23:26:48 +0530

    added 404error image and create 404error message in Membership Page for Now

--- pages/membership.vue
+++ pages/membership.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container>
+  <!-- <v-container> -->
+    <div>
+      <v-card>
+        <v-row no-gutters>
+          <v-col align="center" class="hidden-sm-and-down">
+            <nuxt-img
+            quality="96"
+            format="png"
+            fit="contain"
+            sizes="sm:90vw md:35vw lg:100%"
+            class="align-content-center"
+            src="/Assets/Other/404error.png"/>
+          </v-col>
+        </v-row>
+      </v-card>
+    </div>
 
-  </v-container>
+  <!-- </v-container> -->
 </template>
 
 <script>
