7ec258b2c4d06a801502561cec6d8adf414e386f
SanjulaD <desanjula99@gmail.com>
2022-06-09 13:03:50 +0530

    feat: wie event section

---
+++ content/events/wie.js
@@ -0,0 +1,48 @@
+export const CurrentEvent = null; /// Put null if there is none.
+
+export const PrevEvents = [
+  {
+    title: 'IEEE Day 21',
+    image: '/events/ieeeday21.png',
+    date: '21st March, 2020',
+    description:'Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.',
+    link: 'https://www.facebook.com/events/389076479284226'
+  },
+  {
+    title: 'Road to FullStack',
+    image: '/events/road_to_fullstack.jpg',
+    date: '21st March, 2020',
+    description:'Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.',
+    link: 'https://www.facebook.com/events/1692543347615684/'
+  },
+  {
+    title: 'Professional Guide to CV',
+    image: '/events/professional_guide_to_cv.png',
+    date: '21st March, 2020',
+    description:'Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.',
+    link: 'https://www.facebook.com/events/1016451752425802/'
+  },
+  {
+    title: 'Duothan 1.0',
+    image: '/events/duothon.png',
+    date: '21st March, 2020',
+    description:'Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.',
+    link: 'https://www.facebook.com/events/1358107354543892/'
+  },
+  {
+    title: 'Ayubowan IEEE',
+    image: '/events/ayubowan_ieee.png',
+    date: '21st March, 2020',
+    description:'Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.',
+    link: 'https://www.facebook.com/events/1348980179197235/'
+  },
+  {
+    title: 'Arduino Workshop',
+    image: '/events/adruino.png',
+    date: '21st March, 2020',
+    description:'Minim dolor in amet nulla laboris enim dolore consequat proident fugiat culpa eiusmod.',
+    link: 'https://www.facebook.com/events/1106503076467451/'
+  },
+]
+
+export default { PrevEvents, CurrentEvent };
