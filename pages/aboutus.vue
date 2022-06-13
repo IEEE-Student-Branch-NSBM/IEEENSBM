86ce0cc8ac12054452b5fb6b4a99d8268e350b5d
SanjulaD <desanjula99@gmail.com>
2022-06-13 11:47:39 +0530

    feat: join section

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -72,6 +72,58 @@
         </v-row>
       </v-sheet>
     </v-container>
+    <v-sheet class="pt-8 pb-8">
+      <v-container>
+        <v-row align="center" no-gutters class="DontBreakWords mt-8">
+          <v-col class="px-10 firstCard">
+            <span class="pa-0 text-h2 font-weight-bold"
+              >Our <span style="color: #00629b"> Vision</span>
+            </span>
+            <v-card-text class="pa-0 pb-2 mt-4 text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </v-card-text>
+          </v-col>
+          <v-col>
+            <v-img
+              quality="98"
+              format="webp"
+              fit="contain"
+              class="rounded-lg"
+              height="350"
+              alt="Brand Logo"
+              src="https://picsum.photos/510"
+            />
+          </v-col>
+        </v-row>
+        <v-row align="center" no-gutters class="DontBreakWords mt-8">
+          <v-col class="order-last order-md-first">
+            <v-img
+              quality="98"
+              format="webp"
+              fit="contain"
+              class="rounded-lg"
+              height="350"
+              alt="Brand Logo"
+              src="https://picsum.photos/510"
+            />
+          </v-col>
+          <v-col class="ml-4 px-10">
+            <span class="pa-0 text-h2 font-weight-bold"
+              >Our <span style="color: #00629b">Mission</span>
+            </span>
+            <v-card-text class="pa-0 pb-2 text-justify mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </v-card-text>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
     <JoinUs />
   </div>
 </template>
@@ -81,6 +133,12 @@ import JoinUs from "../components/JoinUs.vue";
 export default {
   name: "aboutus",
   components: { JoinUs },
+  data() {
+    return {
+      showVision: true,
+      showMission: false,
+    };
+  },
 };
 </script>
 
