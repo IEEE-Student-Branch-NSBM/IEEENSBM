ffa9fd7e60fa075b5c0433b2c7f5517a124124fe
SanjulaD <desanjula99@gmail.com>
2022-06-13 12:11:23 +0530

    feat: student branch chapters section

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -1,77 +1,75 @@
 <template>
   <div>
-    <v-container>
-      <v-sheet class="pb-8">
-        <v-container>
-          <v-row align="center" no-gutters class="DontBreakWords mt-8">
-            <v-col class="order-last order-md-first">
-              <v-img
-                quality="98"
-                format="webp"
-                fit="contain"
-                class="rounded-lg"
-                height="350"
-                alt="Brand Logo"
-                :src="'/Assets/Aboutus/aboutIEEE.png'"
-              />
-            </v-col>
-            <v-col class="ml-4 px-10">
-              <v-card-title
-                class="
-                  pa-0
-                  pb-2
-                  font-weight-bold
-                  text-right
-                  float-right
-                  IEEE-title
-                "
-                >About IEEE</v-card-title
-              >
-              <v-card-text class="pa-0 pb-2 right text-right float-right">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </v-card-text>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-sheet>
-      <v-sheet>
-        <v-container>
-          <v-row no-gutters justify="center">
-            <span class="text-h3 font-weight-bold mt-4">
-              About <span style="color: #00629b">IEEE NSBM</span> Student Branch
-            </span>
-          </v-row>
-          <v-row>
-            <div class="my-3 text-center">
+    <v-sheet class="pb-8">
+      <v-container>
+        <v-row align="center" no-gutters class="DontBreakWords mt-8">
+          <v-col class="order-last order-md-first">
+            <v-img
+              quality="98"
+              format="webp"
+              fit="contain"
+              class="rounded-lg"
+              height="350"
+              alt="Brand Logo"
+              :src="'/Assets/Aboutus/aboutIEEE.png'"
+            />
+          </v-col>
+          <v-col class="ml-4 px-10">
+            <v-card-title
+              class="
+                pa-0
+                pb-2
+                font-weight-bold
+                text-right
+                float-right
+                IEEE-title
+              "
+              >About IEEE</v-card-title
+            >
+            <v-card-text class="pa-0 pb-2 right text-right float-right">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat.
-            </div>
-          </v-row>
-        </v-container>
-      </v-sheet>
-      <v-sheet class="mb-10 mt-10">
-        <v-row>
-          <v-col v-for="n in 3" :key="n" class="d-flex child-flex" cols="4">
-            <v-img
-              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-              aspect-ratio="1"
-              class="grey lighten-2"
-            >
-            </v-img>
+              nisi ut aliquip ex ea commodo consequat.
+            </v-card-text>
           </v-col>
         </v-row>
-      </v-sheet>
-    </v-container>
+      </v-container>
+    </v-sheet>
+    <v-sheet>
+      <v-container>
+        <v-row no-gutters justify="center">
+          <span class="text-h3 font-weight-bold mt-4">
+            About <span style="color: #00629b">IEEE NSBM</span> Student Branch
+          </span>
+        </v-row>
+        <v-row>
+          <div class="my-3 text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </div>
+        </v-row>
+      </v-container>
+    </v-sheet>
+    <v-sheet class="mb-10 mt-10">
+      <v-row>
+        <v-col v-for="n in 3" :key="n" class="d-flex child-flex" cols="4">
+          <v-img
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1"
+            class="grey lighten-2"
+          >
+          </v-img>
+        </v-col>
+      </v-row>
+    </v-sheet>
     <v-sheet class="pt-8 pb-8">
       <v-container>
         <v-row align="center" no-gutters class="DontBreakWords mt-8">
@@ -124,6 +122,15 @@
         </v-row>
       </v-container>
     </v-sheet>
+    <v-sheet>
+      <v-container>
+        <v-row no-gutters justify="center mt-4">
+          <span class="text-h3 font-weight-bold">
+            Student Branch Chapters
+          </span>
+        </v-row>
+      </v-container>
+    </v-sheet>
     <JoinUs />
   </div>
 </template>
@@ -132,13 +139,7 @@
 import JoinUs from "../components/JoinUs.vue";
 export default {
   name: "aboutus",
-  components: { JoinUs },
-  data() {
-    return {
-      showVision: true,
-      showMission: false,
-    };
-  },
+  components: { JoinUs }
 };
 </script>
 
