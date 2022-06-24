82601c9f93578baea0a54099fe4ef91259920cd0
SanjulaD <desanjula99@gmail.com>
2022-06-24 22:26:51 +0530

    tania image

--- content/testimonials/WIE.js
+++ content/testimonials/WIE.js
@@ -5,6 +5,13 @@ export const wie_testimonials = [
         description:
             "I'm so proud and humbled to be the first-ever chair to take over the WIE Initiation and I would be thankful for our seniors and members who work hard beside me to get this community to the next level. Uplifting, encouraging, and getting our girls more engaged to do volunteering was the main goal and I believe we were able to achieve it! ",
         img: "/Assets/Excom/Secretary.jpg",
+    },
+    {
+        name: "Tania Wickramaratne",
+        title: "Media coordinator 2021/22",
+        description:
+            "Very effective and productive  ",
+        img: "/Assets/Excom/Tania.png",
     }
 ]
 
