e03bb9dcfb1fed2ebe17eda56d80af9f7aea2f42
srimalonline <dilankasrimal23@gmail.com>
2022-06-28 11:44:45 +0530

    adding event images done

--- content/events/2022/events.js
+++ content/events/2022/events.js
@@ -7,11 +7,10 @@ export const CurrentEvent_2022 = null; /// Put null if there is none.
 },
 */
 
-export const PrevEvents_2022 = [
-    {
+export const PrevEvents_2022 = [{
         id: 1,
         title: 'Duothan 2.0',
-        image: '/events/ieeeday21.png',
+        image: '/events/2022/Duothan 2.0..jpg',
         date: '23rd February, 2022',
         description: 'Duothan 2.0 is a hackathon that is organized by the IEEE Student Branch of NSBM which is competed by groups of two. Each group gets 7 hours for completion and participants will get a break time of about one hour. This hackathon is organized with the explicit purpose of enhancing the coding skills of undergraduates.',
         link: 'https://fb.me/e/1PLtr1COV'
@@ -19,7 +18,7 @@ export const PrevEvents_2022 = [
     {
         id: 2,
         title: 'International Arduino Day Celebration 2022 at NSBM',
-        image: '/events/road_to_fullstack.jpg',
+        image: '/events/2022/International Arduino Day Celebration 2022 at NSBM.jpg',
         date: '23rd March, 2022',
         description: 'Arduino Day is a celebration of the Arduino Community and its achievements. Creativity is the fuel that motivates our users to experiment, build amazing projects, push the boundaries of open-source hardware and software and have a positive impact on people’s lives.',
         link: 'https://fb.me/e/2IoDYeRY3'
@@ -27,7 +26,7 @@ export const PrevEvents_2022 = [
     {
         id: 3,
         title: 'MADFest',
-        image: '/events/road_to_fullstack.jpg',
+        image: '/events/2022/MADFest.jpg',
         date: '25th May, 2022',
         description: 'MAD-Fest is a platform which opens for passionate Mobile Application Developers',
         link: 'https://fb.me/e/2surCGKjG'
