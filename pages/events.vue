065aabeba17504ace8140fc90e85d38216e3c01c
Vinuri Prabodhya <50774562+vinuPAbeywarna@users.noreply.github.com>
2022-10-09 21:27:00 +0530

    Add some events

--- pages/events.vue
+++ pages/events.vue
@@ -10,6 +10,9 @@
         <v-tab>2020</v-tab>
         <v-tab>2019</v-tab>
         <v-tab>2018</v-tab>
+        <v-tab>2017</v-tab>
+        <v-tab>2016</v-tab>
+        <v-tab>WIE Inoguration </v-tab>
       </v-tabs>
 
       <v-tabs-items v-model="tabs">
@@ -83,6 +86,22 @@
             </v-row>
           </v-container>
         </v-tab-item>
+        
+         <v-tab-item>
+          <v-container fluid>
+            <v-row>
+              <v-col
+                v-for="(event, i) in PrevEvents_2017"
+                :key="i"
+                cols="12"
+                md="4"
+              >
+                <AllEvents :event="event"></AllEvents>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
+        
       </v-tabs-items>
     </v-container>
     <JoinUs />
@@ -90,6 +109,7 @@
 </template>
 
 <script>
+import { PrevEvents_2018 } from "@/content/events/2017/events";
 import { PrevEvents_2018 } from "@/content/events/2018/events";
 import { PrevEvents_2019 } from "@/content/events/2019/events";
 import { PrevEvents_2020 } from "@/content/events/2020/events";
@@ -100,6 +120,7 @@ export default {
   name: "events",
   data() {
     return {
+      PrevEvents_2017: [],
       PrevEvents_2018: [],
       PrevEvents_2019: [],
       PrevEvents_2020: [],
@@ -109,6 +130,7 @@ export default {
     };
   },
   mounted() {
+    this.PrevEvents_2018 = PrevEvents_2017;
     this.PrevEvents_2018 = PrevEvents_2018;
     this.PrevEvents_2019 = PrevEvents_2019;
     this.PrevEvents_2020 = PrevEvents_2020;
